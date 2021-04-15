15f89d7d18254a3d1418d62918b6c4cd4d60bfdc
austinandrade <austinmandrade@gmail.com>
2021-04-15 16:34:52 -0400

    Add update testing including sad paths

---
+++ spec/features/admin/merchant_update_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'the admin merchants update' do
+  before(:each) do
+    @merchant_1 = Merchant.create!(name: 'Mike Brown')
+  end
+
+
+  it 'renders prefilled form ' do
+    visit "/admin/merchants/#{@merchant_1.id}/edit"
+
+    expect(find('form')).to have_content('Name')
+    expect(page).to have_field('Name', with: "#{@merchant_1.name}")
+  end
+
+  context "given valid data" do
+    it "submits the edit form and updates the merchant" do
+      visit "/admin/merchants/#{@merchant_1.id}/edit"
+
+      fill_in('Name', with: 'Joey Haandsome')
+      click_button("submit")
+
+      expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
+
+      expect(page).to have_content("successfully updated!")
+      expect(page).to have_content("\nJoey Haandsome")
+      expect(page).to_not have_content('Mike Brown')
+    end
+  end
+
+  context "given invalid data" do
+    it 're-renders the edit form' do
+      visit "/admin/merchants/#{@merchant_1.id}/edit"
+
+      fill_in 'Name', with: ''
+      click_button 'submit'
+
+      expect(page).to have_content("Error: Name can't be blank")
+      expect(page).to have_current_path("/admin/merchants/#{@merchant_1.id}/edit")
+    end
+  end
+end
