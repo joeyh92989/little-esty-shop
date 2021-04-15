ab7f66ed72be456bdd6fa227d2ac9b82628dec9b
austinandrade <austinmandrade@gmail.com>
2021-04-15 14:02:11 -0400

    Move show functionality to index

--- spec/features/admin/merchant_index_spec.rb
+++ spec/features/admin/merchant_index_spec.rb
@@ -14,4 +14,13 @@ RSpec.describe 'the admin merchants index' do
     expect(page).to have_link(@merchant_2.name)
     expect(page).to have_link(@merchant_3.name)
   end
+
+  it 'clicks merchant name and redirects to its show' do
+    visit "/admin/merchants"
+
+    click_link "#{@merchant_1.name}"
+    expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
+
+    expect(page).to have_content(@merchant_1.name)
+  end
 end
