348c897d363cb7e589008ec5d1cee19bd16c4180
Austin A (he/him) <austinmandrade@gmail.com>
2021-04-14 22:05:35 -0400

    Update namespacing

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
-    it { should have_many :dashbaords }
+    it { should have_many :dashboards }
   end
   describe 'validations' do
     it { should validate_presence_of(:name) }
