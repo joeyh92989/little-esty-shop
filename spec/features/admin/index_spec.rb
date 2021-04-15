e01a96008c2d8bc63e7e17c8cd9c84d260bbe701
Joey Haas <josephthaas@gmail.com>
2021-04-14 21:08:55 -0600

    added test and routes for merchants index

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe 'the admin application index' do
 
     expect(page).to have_content("Admin")
   end
+
+  it 'has a link to admin merchant path' do
+    visit "/admin"
+
+    click_link('Merchant Index')
+    expect(current_path).to eq('/admin/merchants') 
+  end
 end
