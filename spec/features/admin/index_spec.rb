2c39c57c57254c88ddaea9178c4ab7fcf0f4a6aa
Joey Haas <josephthaas@gmail.com>
2021-04-14 21:15:52 -0600

    completed testing and links for user story admin dash links

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe 'the admin application index' do
     click_link('Merchant Index')
     expect(current_path).to eq('/admin/merchants') 
   end
+
+  it 'has a link to admin invoice path' do
+    visit "/admin"
+
+    click_link('Invoice Index')
+    expect(current_path).to eq('/admin/invoices') 
+  end
 end
