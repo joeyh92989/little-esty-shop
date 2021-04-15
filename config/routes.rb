e24e0428f39c25b329dc388dbeb2cc5243fc94e1
austinandrade <austinmandrade@gmail.com>
2021-04-14 20:05:03 -0400

    Complete us1; create admin merchants index page, test, and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :merchants
-  get '/admin', to: 'admin#dashboard'
 
+  get '/admin', to: 'admin#dashboard'
   namespace :admin do
     resources :merchants
   end
