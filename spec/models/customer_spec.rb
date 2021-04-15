65964ad4ebe6df2f2caefc3eb48d55f08be3b088
Joey Haas <josephthaas@gmail.com>
2021-04-15 17:03:18 -0600

    storing tests for the merging main

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -10,18 +10,10 @@ RSpec.describe Customer, type: :model do
   end
   
   before(:each) do
-    @customer_1 = Customer.create!(first_name: "Joey", last_name: "Haas")
-    @customer_2 = Customer.create!(first_name: "Austin", last_name: "Andrade")
-    @customer_3 = Customer.create!(first_name: "Joe", last_name: "Ray")
-    @customer_4 = Customer.create!(first_name: "Carlyle", last_name: "Douglas")
-    @customer_5 = Customer.create!(first_name: "Kon", last_name: "Ham")
-    @customer_6 = Customer.create!(first_name: "Andrew", last_name: "Smith")
-    @customer_7 = Customer.create!(first_name: "Olivia", last_name: "Charles")
-    @customer_8 = Customer.create!(first_name: "KK", last_name: "Kudzelich")
-    @customer_9 = Customer.create!(first_name: "Kathy", last_name: "Bui")
-
+ 
     @merchant_1 = create :merchant
     @item_1 = create_list :item, 10, merchant: @merchant_1
+    @invoice_item_1 = create_list :invoice_item, 10
     binding.pry
   end
   describe '::class methods' do
