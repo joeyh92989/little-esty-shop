3516b3ff5c7cf0b5403ff0d057952b9e6b538d95
austinandrade <austinmandrade@gmail.com>
2021-04-15 22:21:06 -0400

    Add merchant model class methods for status checks & Incorporate into admin/merchants_controller

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
   validates :name, presence: true
+
+  def self.enabled
+    where(status: "enabled")
+  end
+
+  def self.disabled
+    where(status: "disabled")
+  end
 end
