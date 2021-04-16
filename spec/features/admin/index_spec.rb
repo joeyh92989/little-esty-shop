8c103784a97b3425183a825a4bc85102fa5352e9
Joey Haas <josephthaas@gmail.com>
2021-04-16 14:17:34 -0600

    created view to display the top 5 customers and their succesful transactions. This is tested in the feature spec for admin index

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'the admin application index' do
-  # before(:each) do
-  # end
+  before(:each) do
+    @customer = create_list(:customer, 6)
+    @invoice = @customer.each{|customer| create :invoice, customer: customer}
+    @transaction_1 = create_list :transaction, 10, invoice: @customer[0].invoices.first
+    @transaction_2 = create_list :transaction, 10, invoice: @customer[1].invoices.first
+    @transaction_3 = create_list :transaction, 10, invoice: @customer[2].invoices.first
+    @transaction_4 = create_list :transaction, 10, invoice: @customer[3].invoices.first
+    @transaction_5 = create_list :transaction, 10, invoice: @customer[4].invoices.first
+    @transaction_6 = create_list :transaction, 10, result: 1, invoice: @customer[5].invoices.first
+  end
 
   it 'displays admin dashboard ' do
     visit "/admin"
@@ -22,5 +30,41 @@ RSpec.describe 'the admin application index' do
 
     click_link('Invoice Index')
     expect(current_path).to eq('/admin/invoices') 
+  end
+  it 'lists the top 5 customers with their number of succesful transctions' do
+    visit "/admin"
+    save_and_open_page
+
+    expect(page).to_not have_content(@customer[5].first_name)
+
+    within "#customer-#{@customer[0].id}" do
+      expect(page).to have_content(@customer[0].first_name)
+      expect(page).to have_content(@customer[0].last_name)
+      expect(page).to have_content(@customer[0].number_of_succesful_transactions)
+    end
+    within "#customer-#{@customer[1].id}" do
+      expect(page).to have_content(@customer[1].first_name)
+      expect(page).to have_content(@customer[1].last_name)
+      expect(page).to have_content(@customer[1].number_of_succesful_transactions)
+
+    end
+    within "#customer-#{@customer[2].id}" do
+      expect(page).to have_content(@customer[2].first_name)
+      expect(page).to have_content(@customer[2].last_name)
+      expect(page).to have_content(@customer[2].number_of_succesful_transactions)
+
+    end
+    within "#customer-#{@customer[3].id}" do
+      expect(page).to have_content(@customer[3].first_name)
+      expect(page).to have_content(@customer[3].last_name)
+      expect(page).to have_content(@customer[3].number_of_succesful_transactions)
+
+    end
+    within "#customer-#{@customer[4].id}" do
+      expect(page).to have_content(@customer[4].first_name)
+      expect(page).to have_content(@customer[4].last_name)
+      expect(page).to have_content(@customer[4].number_of_succesful_transactions)
+
+    end
   end
 end
