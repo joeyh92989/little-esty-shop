d4daca74aa15107e6f77836781d51222d159edf4
austinandrade <austinmandrade@gmail.com>
2021-04-15 22:18:40 -0400

    Change string enum attribute to integer to work properly

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_232235) do
   create_table "transactions", force: :cascade do |t|
     t.string "credit_card_number"
     t.date "credit_card_expiration_date"
-    t.string "result"
+    t.integer "result"
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
