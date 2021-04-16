6387f1213901126a23a2a931e1fb4daaad66cb86
austinandrade <austinmandrade@gmail.com>
2021-04-15 23:34:18 -0400

    Add additional update method for status updates

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -5,7 +5,7 @@ class MerchantsController < ApplicationController
 
   def update
     merchant = Merchant.find(params[:id])
-    if merchant.update(merchant_params)
+    if merchant.update(merchant_name_params)
       redirect_to "/admin/merchants/#{merchant.id}",
       notice: "successfully updated!"
     else
@@ -14,9 +14,22 @@ class MerchantsController < ApplicationController
     end
   end
 
+  def update_status
+    merchant = Merchant.find(params[:id])
+    if params[:status] == 'disable'
+      merchant.update(status: "disabled")
+      redirect_to "/admin/merchants",
+      notice: "#{merchant.name} has been disabled!"
+    elsif params[:status] == 'enable'
+      merchant.update(status: "enabled")
+      redirect_to "/admin/merchants",
+      notice: "#{merchant.name} has been enabled!"
+    end
+  end
+
   private
 
-  def merchant_params
+  def merchant_name_params
     params.permit(:id, :name)
   end
 end
