63589301708329cbf77aa5d196b6c7837957ecff
Joey Haas <josephthaas@gmail.com>
2021-04-17 13:49:28 -0600

    created feature test for displaying total revenue within the admin invoice show page

--- spec/features/admin/invoice/show_spec.rb
+++ spec/features/admin/invoice/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'the admin invoice show' do
   before(:each) do
     @invoice= create :invoice
+    @invoice_items = create_list :invoice_items, 10, invoice: @invoice
   end
 
   it 'admin invoice show ' do
@@ -16,6 +17,15 @@ RSpec.describe 'the admin invoice show' do
       expect(page).to have_content(@invoice.created_at.to_time.strftime(' %A, %B %e, %Y'))
       expect(page).to have_content(@invoice.customer.first_name)
       expect(page).to have_content(@invoice.customer.last_name)
+    end
+  end
+  it 'displays the total revenue within the admin invoice show ' do
+    visit "/admin/invoices/#{@invoice.id}"
+
+
+    within "#invoice-#{@invoice.id}" do
+      expect(page).to have_content(@invoice.total_revenue)
+      
     end
   end
 end
