7d1266f0ae65c5a5deb1917db986fef321c56749
austinandrade <austinmandrade@gmail.com>
2021-04-16 23:01:12 -0400

    Move member function to admin namespace from merchants resource for restfulness

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,17 @@ Rails.application.routes.draw do
   get "/admin", to: 'admin/dashboard#index'
 
   resources :merchants do
-    member do
-      post :update_status
-    end
     resources :invoices
     resources :items
     resources :dashboard, only: [:index]
   end
 
   namespace :admin do
-    resources :merchants
+    resources :merchants do
+      member do
+        post :update_status
+      end
+    end
     resources :invoices
   end
 end
