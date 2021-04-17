09393a8e9dd146ef12a61876b4da75d1ad29eea4
austinandrade <austinmandrade@gmail.com>
2021-04-16 22:59:26 -0400

    Move methods to admin_merchants_controller for restfulness

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,34 +2,4 @@ class MerchantsController < ApplicationController
 
   def show
   end
-
-  def update
-    merchant = Merchant.find(params[:id])
-    if merchant.update(merchant_name_params)
-      redirect_to "/admin/merchants/#{merchant.id}",
-      notice: "successfully updated!"
-    else
-      redirect_to "/admin/merchants/#{merchant.id}/edit",
-      alert: "Error: #{error_message(merchant.errors)}"
-    end
-  end
-
-  def update_status
-    merchant = Merchant.find(params[:id])
-    if params[:status] == 'disable'
-      merchant.update(status: "disabled")
-      redirect_to "/admin/merchants",
-      notice: "#{merchant.name} has been disabled!"
-    elsif params[:status] == 'enable'
-      merchant.update(status: "enabled")
-      redirect_to "/admin/merchants",
-      notice: "#{merchant.name} has been enabled!"
-    end
-  end
-
-  private
-
-  def merchant_name_params
-    params.permit(:id, :name)
-  end
 end
