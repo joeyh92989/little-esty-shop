3bacae338804522a3bacf67da5a175de2bbe1768
austinandrade <austinmandrade@gmail.com>
2021-04-18 01:40:47 -0400

    Create top_5 and single merchant total revenue generated methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,11 +8,27 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
   validates :status, presence: true
 
+  def self.top_5_by_revenue
+    joins(:transactions, :invoice_items)
+    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
+    .group(:id)
+    .where('transactions.result = ?', 0)
+    .order('total_revenue desc')
+    .limit(5)
+  end
+
   def self.enabled
     where(status: "enabled")
   end
 
   def self.disabled
     where(status: "disabled")
+  end
+
+  def total_revenue
+    invoice_items.group(:id)
+    .sum('invoice_items.quantity * invoice_items.unit_price')
+    .values
+    .sum
   end
 end
