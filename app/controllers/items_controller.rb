0daf5cce71b5490f0d723dda09a8521774fc54e3
austinandrade <austinmandrade@gmail.com>
2021-04-19 23:57:17 -0400

    Update edit method to incorporate both merchant and item in view, and Create update method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,25 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(item_params[:id])
+    @merchant = Merchant.find(item_params[:merchant_id])
+  end
+
+  def edit
+    @item = Item.find(item_params[:id])
+    @merchant = Merchant.find(item_params[:merchant_id])
+  end
+
+  def update
+    merchant = Merchant.find(item_params[:merchant_id])
+    item = Item.find(item_params[:id])
+
+    if item.update(item_params)
+      redirect_to "/merchants/#{merchant.id}/items/#{item.id}",
+      notice: "successfully updated!"
+    else
+      redirect_to "/merchants/#{merchant.id}/items/#{item.id}/edit",
+      alert: "Error: #{error_message(item.errors)}"
+    end
   end
 
   private
