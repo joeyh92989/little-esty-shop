49df8046e669d76a391f78cd47dc39e90571a01a
Joey Haas <josephthaas@gmail.com>
2021-04-19 23:29:21 -0600

    added test for top 5 item class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,7 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates :name, :description, :unit_price, :merchant_id, presence: true
-
+  def self.top_5
+    Item.select(:name,:id).joins(:invoice_items).joins(invoices: :transactions).where('transactions.result = 0').group(:id).select('SUM(invoice_items.unit_price * invoice_items.quantity) as tot').order('tot desc')
+  end
 end
