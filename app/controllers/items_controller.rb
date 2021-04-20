7ef10cbb04388184f9c9ded3705b2a88cb53eef5
austinandrade <austinmandrade@gmail.com>
2021-04-19 22:22:59 -0400

    Create item index, display each item, and include link to their page and test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
-
   def index
-    @merchant = Merchant.find(params[:merchant_id])
-    @items = @merchant.items
+    @merchant = Merchant.find(item_params[:merchant_id])
   end
 
+  def show
+  
+  end
+
+  private
+
+  def item_params
+    params.permit(:id, :name, :description, :unit_price, :merchant_id)
+  end
 end
