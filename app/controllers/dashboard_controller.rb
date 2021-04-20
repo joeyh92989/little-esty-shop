3eed15646d00be11b7fdb520a00bed34dbcc35f4
Joe Ray <jray100.jr@gmail.com>
2021-04-20 13:43:04 -0600

    Update customer model for merch method and add instance var for dashb

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
 
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @top_customers = @merchant.top_5_by_transactions
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
   validates :first_name, :last_name, presence: true
 
   def self.top_5
