f7536eb751923938f1a488a37d608fa4ead8b342
austinandrade <austinmandrade@gmail.com>
2021-04-20 00:41:43 -0400

    Add disable and enable button and distinctions for enabled and disabled items and test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(item_params[:merchant_id])
+    @enabled_items = Merchant.find(item_params[:merchant_id]).items.enabled
+    @disabled_items = Merchant.find(item_params[:merchant_id]).items.disabled
   end
 
   def show
@@ -23,6 +25,20 @@ class ItemsController < ApplicationController
     else
       redirect_to "/merchants/#{merchant.id}/items/#{item.id}/edit",
       alert: "Error: #{error_message(item.errors)}"
+    end
+  end
+
+  def update_status
+    merchant = Merchant.find(item_params[:merchant_id])
+    item = Item.find(item_params[:id])
+    if params[:status] == 'disable'
+      item.update(status: "disabled")
+      redirect_to "/merchants/#{merchant.id}/items",
+      notice: "#{item.name} has been disabled!"
+    elsif params[:status] == 'enable'
+      item.update(status: "enabled")
+      redirect_to "/merchants/#{merchant.id}/items",
+      notice: "#{item.name} has been enabled!"
     end
   end
 
