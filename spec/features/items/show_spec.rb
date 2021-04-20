ec9a40a9c4cfb0f0c9c53e52693ad64b9a403f22
austinandrade <austinmandrade@gmail.com>
2021-04-19 23:55:15 -0400

    Add link_to update, redirecting to edit page and test

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'merchant items show' do
     @item_3 = create :item, merchant: @merchant[2]
   end
 
-  it "clicks an item and redirects to show" do
+  it "displays item attributes" do
     visit "/merchants/#{@merchant[0].id}/items/#{@item_1.id}"
 
     expect(page).to have_content("Name: #{@item_1.name}")
@@ -17,5 +17,18 @@ RSpec.describe 'merchant items show' do
     expect(page).to have_content("Current Selling Price: #{@item_1.unit_price}")
 
     expect(page).to_not have_content(@item_2.name)
+  end
+
+  it "clicks update link and redirects to edit page" do
+    visit "/merchants/#{@merchant[0].id}/items/#{@item_1.id}"
+
+    expect(page).to have_link("Update")
+    click_link "Update"
+
+    expect(page).to have_current_path("/merchants/#{@merchant[0].id}/items/#{@item_1.id}/edit")
+    # And I see a form filled in with the existing item attribute information
+    # When I update the information in the form and I click ‘submit’
+    # Then I am redirected back to the item show page where I see the updated information
+    # And I see a flash message stating that the information has been successfully updated.
   end
 end
