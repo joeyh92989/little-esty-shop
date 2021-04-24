86f70e2318f7525df504e58d3167de252e13ad5a
Joey Haas <josephthaas@gmail.com>
2021-04-24 11:14:02 -0600

    removed and unused relation between invoiceitem and merchant

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,6 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
-  has_many :merchants, through: :item
 
   enum status: [ :"pending", :"packaged", :"shipped" ]
 
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
-    it { should have_many(:merchants).through(:items) }
 
   end
   describe 'validations' do
