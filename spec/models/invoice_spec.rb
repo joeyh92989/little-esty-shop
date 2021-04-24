7fd4baf214132af5652d02de478409af4f4399cd
Joey Haas <josephthaas@gmail.com>
2021-04-24 11:45:26 -0600

    Added all required associations to model tests for the new BulkDiscount model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :transactions }
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
-
+    it { should have_many(:bulk_discounts).through(:invoice_items) }
   end
   describe 'validations' do
     it { should validate_presence_of(:status) }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:bulk_discounts).through(:merchant) }
   end
   describe 'validations' do
     it { should validate_presence_of(:name) }
