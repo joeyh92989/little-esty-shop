9faf5c4cc6042aedde9fc4b66ca492c935bad589
Joey Haas <josephthaas@gmail.com>
2021-04-24 00:38:46 -0600

    updated files for readability

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'the admin application index' do
+RSpec.describe "the admin application index" do
   before(:each) do
     @customer = create_list(:customer, 6)
-    @invoice_1 = @customer.each{|customer| create :invoice, customer: customer}
+    @invoice_1 = @customer.each { |customer| create :invoice, customer: customer }
     @transaction_1 = create_list :transaction, 10, invoice: @customer[0].invoices.first
     @transaction_2 = create_list :transaction, 10, invoice: @customer[1].invoices.first
     @transaction_3 = create_list :transaction, 10, invoice: @customer[2].invoices.first
@@ -14,31 +14,29 @@ RSpec.describe 'the admin application index' do
     @invoice_items_1 = create_list :invoice_item, 10, status: 2, invoice: @invoice_2[0]
     @invoice_items_2 = create_list :invoice_item, 10, status: 0, invoice: @invoice_2[1]
     @invoice_items_3 = create_list :invoice_item, 10, status: 1, invoice: @invoice_2[2]
-    
   end
 
-  it 'displays admin dashboard ' do
+  it "displays admin dashboard " do
     visit "/admin"
 
     expect(page).to have_content("Admin")
   end
 
-  it 'has a link to admin merchant path' do
+  it "has a link to admin merchant path" do
     visit "/admin"
 
-    click_link('Merchant Index')
-    expect(current_path).to eq('/admin/merchants') 
+    click_link("Merchant Index")
+    expect(current_path).to eq("/admin/merchants")
   end
 
-  it 'has a link to admin invoice path' do
+  it "has a link to admin invoice path" do
     visit "/admin"
 
-    click_link('Invoice Index')
-    expect(current_path).to eq('/admin/invoices') 
+    click_link("Invoice Index")
+    expect(current_path).to eq("/admin/invoices")
   end
-  it 'lists the top 5 customers with their number of succesful transctions' do
+  it "lists the top 5 customers with their number of succesful transctions" do
     visit "/admin"
-    
 
     expect(page).to_not have_content(@customer[5].first_name)
 
@@ -51,30 +49,26 @@ RSpec.describe 'the admin application index' do
       expect(page).to have_content(@customer[1].first_name)
       expect(page).to have_content(@customer[1].last_name)
       expect(page).to have_content(@customer[1].number_of_succesful_transactions)
-
     end
     within "#customer-#{@customer[2].id}" do
       expect(page).to have_content(@customer[2].first_name)
       expect(page).to have_content(@customer[2].last_name)
       expect(page).to have_content(@customer[2].number_of_succesful_transactions)
-
     end
     within "#customer-#{@customer[3].id}" do
       expect(page).to have_content(@customer[3].first_name)
       expect(page).to have_content(@customer[3].last_name)
       expect(page).to have_content(@customer[3].number_of_succesful_transactions)
-
     end
     within "#customer-#{@customer[4].id}" do
       expect(page).to have_content(@customer[4].first_name)
       expect(page).to have_content(@customer[4].last_name)
       expect(page).to have_content(@customer[4].number_of_succesful_transactions)
-
     end
   end
-  it 'lists invoices with unshipped items' do
+  it "lists invoices with unshipped items" do
     visit "/admin"
-    
+
     expect(page).to_not have_content(@invoice_2[0].id)
 
     within "#invoice-#{@invoice_2[1].id}" do
--- spec/features/admin/merchant_show_spec.rb
+++ spec/features/admin/merchant_show_spec.rb
@@ -1,20 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'the admin merchants show' do
+RSpec.describe "the admin merchants show" do
   before(:each) do
     @merchant_1 = create(:merchant)
   end
 
-    it 'renders an update button' do
-      visit "/admin/merchants/#{@merchant_1.id}"
+  it "renders an update button" do
+    visit "/admin/merchants/#{@merchant_1.id}"
 
-      expect(page).to have_button("update")
-    end
+    expect(page).to have_button("update")
+  end
 
-    it 'clicks update button and redirects to edit page' do
-      visit "/admin/merchants/#{@merchant_1.id}"
+  it "clicks update button and redirects to edit page" do
+    visit "/admin/merchants/#{@merchant_1.id}"
 
-      click_button("update")
-      expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}/edit")
-    end
+    click_button("update")
+    expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}/edit")
+  end
 end
